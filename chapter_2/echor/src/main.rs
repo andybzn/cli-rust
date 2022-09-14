abcb0d14978081a2e6996a5f17955336de897e27
Andy Baizon <dev@baizon.uk>
2022-09-14 21:37:53 +0100

    basic tests
    added .allow_invalid_utf8(true) to main program as tests fail otherwise. no idea why.

--- chapter_2/echor/src/main.rs
+++ chapter_2/echor/src/main.rs
@@ -11,7 +11,8 @@ fn main() {
             .value_name("TEXT")
             .help("Input text")
             .required(true)
-            .min_values(1),
+            .min_values(1)
+            .allow_invalid_utf8(true),
       )
       .arg(
         Arg::with_name("omit_newline")
@@ -24,5 +25,5 @@ fn main() {
   let text = matches.values_of_lossy("text").unwrap();
   let omit_newline = matches.is_present("omit_newline");
 
-  print!("{}", text.join(" "), if omit_newline { "" } else { "\n" });
+  print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
