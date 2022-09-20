a012a027cee27c345432ed2b94cdce4e1195453e
Andy Baizon <dev@baizon.uk>
2022-09-20 22:52:00 +0100

    added hello1 test

--- chapter_2/echor/tests/cli.rs
+++ chapter_2/echor/tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 #[test]
 fn dies_no_args() {
@@ -13,4 +14,12 @@ fn dies_no_args() {
 fn runs() {
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
 }
